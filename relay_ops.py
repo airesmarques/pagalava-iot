fda94d181f03c961d5c136d2a6d2e11e2d4af3aa
yimai@washstation.net <yimai@washstation.net>
2024-02-25 00:53:35 +0000

    updated relays

--- relay_ops.py
+++ relay_ops.py
@@ -1,19 +1,21 @@
 import RPi.GPIO as GPIO
 import time
+import json
 
 # Define a dictionary to map relay labels to GPIO pins
 
-relay_mapping_v0 = {
-    7: 9,
-    8: 10,
-    9: 11,
-    10: 12,
-    11: 13,
-    12: 14,
-    13: 15,
-    14: 16
-}
+# relay_mapping_v0 = {
+#     7: 9,
+#     8: 10,
+#     9: 11,
+#     10: 12,
+#     11: 13,
+#     12: 14,
+#     13: 15,
+#     14: 16
+# }
 
+used_pins = [9, 10, 11, 12, 13, 14, 15, 16, 20, 21, 17]
 
 # L2 - Avenida 
 # Maps 
@@ -68,14 +70,54 @@ Module 3
 
 
 GPIO.setmode(GPIO.BCM)  # Use BCM GPIO numbering
-GPIO.setup(list(relay_pins_v0.values()), GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(used_pins, GPIO.OUT, initial=GPIO.HIGH)
+
+
+def relay_mapping(
+    file_path='config.json'):
+    """
+    Loads relay mapping from a JSON configuration file and converts it into a dictionary with integer keys and values.
+
+    :param file_path: The path to the configuration JSON file.
+    :return: A dictionary with integer keys and values representing the relay mapping.
+    """
+    try:
+        with open(file_path, 'r') as file:
+            config = json.load(file)
+            # Convert keys and values to integers
+            relay_mapping = {int(key): int(value) for key, value in config.items()}
+            return relay_mapping
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return {}
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from file: {file_path}")
+        return {}
+    except ValueError:
+        print("Invalid format in config.json. Keys and values must be convertible to integers.")
+        return {}
+
+def get_relay_number(
+    machine_id : int,
+    file_path='config.json'):
+    """
+    Retrieves the relay number for a given machine number using the relay mapping from a JSON configuration file.
+
+    :param machine_number: The machine number for which to retrieve the relay number.
+    :param file_path: The path to the configuration JSON file.
+    :return: The relay number corresponding to the given machine number, or None if not found.
+    """
+    mapping = relay_mapping(file_path)
+    return mapping.get(machine_id, None)
 
 
 # Function to control a relay
-def control_relay(relay_label, state):
+def control_relay(
+    relay_number: int,
+    state):
 
-    pin = relay_pins_v0[relay_label]
-    print("preparing for",relay_label, pin)
+    pin = relay_pins_v0[relay_number]
+    print("preparing for",relay_number, pin)
     GPIO.output(pin, state)
 
     #GPIO.cleanup()
@@ -86,15 +128,14 @@ def activate_machine(
     number_of_impulses: int = 1):
     # Setup GPIO
     
-    relay_pin = relay_mapping_v0.get(machine_id)
+    relay_number = get_relay_number(machine_id)
     
     for i in range(number_of_impulses):   
         # Activate the machine by setting the relay to the energized state (low)
-        control_relay(relay_pin, GPIO.LOW)
-
+        control_relay(relay_number, GPIO.LOW)
         # Deactivate after 2 seconds
         time.sleep(1)
-        control_relay(relay_pin, GPIO.HIGH)
+        control_relay(relay_number, GPIO.HIGH)
         time.sleep(1)
 
 
